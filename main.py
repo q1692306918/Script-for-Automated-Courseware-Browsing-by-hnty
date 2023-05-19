ceeedeb82813f0df1d02a95e0a7ff340a1d92789
q1692306918 <z1692306918@163.com>
2023-05-19 13:56:42 +0800

    逻辑改动

--- main.py
+++ main.py
@@ -1,86 +1,91 @@
 import threading
 import time
-
 import easygui
 from browser import browser_handless_edge, close_app
 from handlers import handle_mp4, handle_ptx
 from gui import create_root_window, get_activation_code, get_url
-from utils import update_status_text, check_activation_code
+from utils import update_status_text, check_activation_code, submit, getUser
 
 
-def main(driver, root, status_text, exit_button):
-    exit_thread = threading.Event()  
-    driver.get(url) 
-    driver.implicitly_wait(10)  
+def main(driver, root, status_text, exit_button, user_status):
+    user = getUser(url, driver)
 
-    exit_button.config(command=lambda: close_app(driver, root, exit_thread))  
+    exit_thread = threading.Event()
+    driver.get(url)
+    driver.implicitly_wait(10)
+    exit_button.config(command=lambda: close_app(driver, root, exit_thread))
 
-    while not exit_thread.is_set(): 
+    while not exit_thread.is_set():
         try:
-            divs = driver.find_elements_by_css_selector('._1QoJyBnLPXjLp8cV61PPJe') 
-            for div in divs:    
-                name = div.find_element_by_css_selector('._3dYlgmbXq9p-8xwwf_goNT').text    
-                status = div.find_element_by_css_selector('._3HzEldvxtmCRMKy_ELaND8 :nth-child(2)').text[-1]    
-                status1 = div.find_element_by_css_selector('._3HzEldvxtmCRMKy_ELaND8 :nth-child(2)').text[-2]   
-                update_status_text(status_text, f'未读{status}')  
+            divs = driver.find_elements_by_css_selector('._1QoJyBnLPXjLp8cV61PPJe')
+            for div in divs:
+                name = div.find_element_by_css_selector('._3dYlgmbXq9p-8xwwf_goNT').text
+                class_ = name.split('-')[1]
+                if user_status != 'ERROR: user already exists':
+                    print(user_status)
+                    user_status = submit(user, class_, user_status)
+                status = div.find_element_by_css_selector('._3HzEldvxtmCRMKy_ELaND8 :nth-child(2)').text[-1]
+                status1 = div.find_element_by_css_selector('._3HzEldvxtmCRMKy_ELaND8 :nth-child(2)').text[-2]
+                update_status_text(status_text, f'未读{status}')
 
-                if status == '0' and status1 != '1' and status1 != '2': 
-                    update_status_text(status_text, f'{name}已完成！')  
-                    continue    
-                update_status_text(status_text, f'{name}开始')    
-                div.click() 
-                break   
-            while True: 
-                card = driver.find_element_by_css_selector('._1UfYCzyzkv94Zbsyr_Hoc0')  
-                update_status_text(status_text, card.text)  
-                status = card.find_element_by_css_selector('._1GoZ9bt9hJQ5yLFgOgeVOI').text 
-                update_status_text(status_text, status) 
+                if status == '0' and status1 != '1' and status1 != '2':
+                    update_status_text(status_text, f'{name}已完成！')
+                    continue
+                update_status_text(status_text, f'{name}开始')
+                div.click()
+                break
 
-                if status == '已读':  
-                    update_status_text(status_text, '全部已完成')    
-                    driver.get(url) 
-                    break   
+            while True:
+                card = driver.find_element_by_css_selector('._1UfYCzyzkv94Zbsyr_Hoc0')
+                update_status_text(status_text, card.text)
+                status = card.find_element_by_css_selector('._1GoZ9bt9hJQ5yLFgOgeVOI').text
+                update_status_text(status_text, status)
 
-                type_work = card.find_element_by_css_selector('.dPphweSVDC1Hf6EnOO71w').text[-3:]   
-                update_status_text(status_text, type_work)  
-                card.click()    
-                time.sleep(0.5) 
-                driver.find_element_by_css_selector('.weui-actionsheet__cell').click()  
+                if status == '已读':
+                    update_status_text(status_text, '全部已完成')
+                    driver.get(url)
+                    break
 
-                if type_work == 'mp4':  
-                    handle_mp4(driver, status_text) 
-                elif type_work == 'ptx':    
-                    handle_ptx(driver, status_text) 
+                type_work = card.find_element_by_css_selector('.dPphweSVDC1Hf6EnOO71w').text[-3:]
+                update_status_text(status_text, type_work)
+                card.click()
+                time.sleep(0.5)
+                driver.find_element_by_css_selector('.weui-actionsheet__cell').click()
+
+                if type_work == 'mp4':
+                    handle_mp4(driver, status_text)
+                elif type_work == 'ptx':
+                    handle_ptx(driver, status_text)
 
                 time.sleep(60)
-                driver.find_element_by_css_selector('._3IXHvJLszKa_-s4kn1kPlT').click() 
-                driver.refresh()    
-                time.sleep(3)   
+                driver.find_element_by_css_selector('._3IXHvJLszKa_-s4kn1kPlT').click()
+                driver.refresh()
+                time.sleep(3)
             root.update()
-        except: 
-            if exit_thread.is_set():  
-                break   
-            driver.save_screenshot('结果.png')    
-            update_status_text(status_text, '出了点问题')    
-            main(driver, root, status_text, exit_button)    
+        except:
+            if exit_thread.is_set():
+                break
+            driver.save_screenshot('结果.png')
+            update_status_text(status_text, '出了点问题')
+            main(driver, root, status_text, exit_button, user_status)
 
 
 if __name__ == "__main__":
-    while True: 
-        activation_code = get_activation_code() 
+    while True:
+        activation_code = get_activation_code()
         print(check_activation_code(activation_code))
-        if check_activation_code(activation_code) == "SUCCESS": 
-            easygui.msgbox("本次激活成功！")   
-            break   
+        if check_activation_code(activation_code) == "SUCCESS":
+            easygui.msgbox("本次激活成功！")
+            break
         else:
-            easygui.msgbox(check_activation_code(activation_code))  
+            easygui.msgbox(check_activation_code(activation_code))
+    user_status = ''
+    driver = browser_handless_edge()
+    url = get_url()
+    root, status_text, exit_button = create_root_window()
+    update_status_text(status_text, "Program start")
 
-    driver = browser_handless_edge()    
-    url = get_url() 
-    root, status_text, exit_button = create_root_window()   
-    update_status_text(status_text, "Program start")    
+    thread = threading.Thread(target=main, args=(driver, root, status_text, exit_button, user_status))
+    thread.start()
 
-    thread = threading.Thread(target=main, args=(driver, root, status_text, exit_button))   
-    thread.start()  
-
-    root.mainloop() 
+    root.mainloop()
--- utils.py
+++ utils.py
@@ -13,7 +13,23 @@ def update_status_text(status_text, text):
     status_text.see(tk.END) 
     logging.info(text)  
 
+def getUser(url,driver):
+    openID =url[url.rfind('=') + 1:]
+    print(openID)
+    homepage = f'http://www.hntyxxh.com/wechat2-ssr/?openid={openID}&from=wzj'
+    print(homepage)
+    driver.get(homepage)
+    user = driver.find_element_by_css_selector('.EXQcJu5rkwcRFryNNYIQV').text
+    return user
 
+
+def submit(user,class_,user_status):
+    url = 'https://activecode-1-9gyjmiqg675db947-1310894818.ap-shanghai.app.tcloudbase.com/user_info'
+    data = {'user': user, 'class': class_}
+    response = requests.get(url, json=data)
+    print(response.text)
+    user_status=response.text
+    return user_status
 def check_activation_code(activation_code): 
     url1 = "https://activecode-1-9gyjmiqg675db947-1310894818.ap-shanghai.app.tcloudbase.com/test"  
     payload = {"activation_code": activation_code}  
